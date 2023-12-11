129860ff238353d8f599b97d5fee49b2d412dc2c
De2Viator <troshenkoyura@gmail.com>
2023-12-12 01:03:51 +0200

    add fix

--- main.cpp
+++ main.cpp
@@ -34,33 +34,6 @@ BigNumber generate_p() {
     return random_number;
 
 }
-
-PrimeNumbers defactor_number(BigNumber number) {
-    PrimeNumbers prime_numbers;
-    for(unsigned long long divisor = 2; divisor<=number; ++divisor) {
-        while (number % divisor == 0) {
-            number /= divisor;
-            if(prime_numbers[divisor]) {
-                ++prime_numbers[divisor];
-            } else {
-                prime_numbers[divisor] = 1;
-            }
-        }
-    }
-
-    return prime_numbers;
-}
-unsigned long long euler_function(BigNumber number) {
-    PrimeNumbers prime_numbers = defactor_number(number);
-    unsigned long long prime_numbers_amount = 1;
-    for (auto & prime_number : prime_numbers) {
-        unsigned long long default_number = prime_number.first;
-        unsigned long long exponent = prime_number.second;
-        prime_numbers_amount *= pow(default_number, exponent)  - pow(default_number, exponent-1);
-    }
-    return prime_numbers_amount;
-}
-
 BigNumber mod_pow(BigNumber base, BigNumber exponent, BigNumber modulus) {
     if (exponent == 0) {
         return 1;
@@ -79,30 +52,48 @@ BigNumber pow (BigNumber base, BigNumber exponent) {
     }
     return result;
 }
-std::vector<BigNumber> find_primitive_roots(const boost::multiprecision::cpp_int& number) {
-    unsigned long long euler_number = euler_function(number);
-    PrimeNumbers defactored_number = defactor_number(euler_number);
-    std::vector<BigNumber> exponents;
-    std::vector<BigNumber> primitive_roots;
-    for (auto & it : defactored_number) {
-        exponents.push_back(it.first);
-    }
+std::vector<BigNumber> factorize(BigNumber n) {
+    std::vector<BigNumber> factors;
 
-    for (unsigned long long i = 2; i < euler_number; ++i) {
-        bool is_primitive = true;
-        for (BigNumber exponent : exponents) {
-            BigNumber mod_pow_result = mod_pow(i,exponent, number);
-            if(mod_pow_result == 1) {
-                is_primitive = false;
-                break;
-            }
-        }
-        if(is_primitive) {
-            primitive_roots.push_back(i);
+    for (BigNumber i = 2; i * i <= n; ++i) {
+        while (n % i == 0) {
+            factors.push_back(i);
+            n /= i;
         }
     }
 
-    return primitive_roots;
+    if (n > 1) {
+        factors.push_back(n);
+    }
+
+    return factors;
+}
+
+bool is_primitive_root(BigNumber g, BigNumber p) {
+    if (g <= 1 || g >= p) {
+        return false;
+    }
+
+    BigNumber phi = p - 1;
+    std::vector<BigNumber> factors = factorize(phi);
+
+    for (const auto& factor : factors) {
+        if (mod_pow(g, phi / factor, p) == 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<BigNumber> find_primitive_roots(BigNumber p) {
+    std::vector<BigNumber> roots;
+    for (BigNumber g = 2; g < p; ++g) {
+        if (is_primitive_root(g, p)) {
+            roots.push_back(g);
+        }
+    }
+    return roots;
 }
 BigNumber generate_random_number(BigNumber lower, BigNumber upper) {
     boost::random::uniform_int_distribution<BigNumber> dist(lower, upper);
@@ -140,40 +131,25 @@ BigNumber hex_to_bn(const std::string& hex) {
     ss >> numbered_hash;
     return numbered_hash;
 }
-Signing sign_message(BigNumber p, BigNumber g, BigNumber m, BigNumber a) {
+Signing sign_message(BigNumber p, BigNumber g, std::string m, BigNumber a) {
     BigNumber p_minus_one = p-1;
     BigNumber k = generate_random_number(2,p_minus_one);
-    BigNumber inversed_k = boost::integer::mod_inverse(k, p_minus_one);
-    std::cout<<"p-1: "<<p_minus_one<<std::endl;
-    std::cout<<"k: "<<k<<std::endl;
-    std::cout<<"k^-1: "<<inversed_k<<std::endl;
+    BigNumber inversed_k = boost::integer::mod_inverse(k, p_minus_one);;
     BigNumber r = mod_pow(g, k ,p);
-    std::cout<<"r: "<<r<<std::endl;
-    std::string H = sha256( boost::multiprecision::to_string(m));
-    std::cout<<"Hash:"<<H<<std::endl;
+    std::string H = sha256( m);
     BigNumber numbered_hash = hex_to_bn(H);
-    std::cout<<"H: "<<numbered_hash<<std::endl;
-    BigNumber test = (numbered_hash - a*r) * inversed_k;
     BigNumber s = ((numbered_hash - a*r) * inversed_k) % p_minus_one;
-    std::cout<<"s: "<<s<<std::endl;
     return {r,s};
 }
-BigNumber verificate_message(BigNumber b, BigNumber p, BigNumber m, BigNumber s, BigNumber r, BigNumber g) {
+BigNumber verificate_message(BigNumber b, BigNumber p, std::string m, BigNumber s, BigNumber r, BigNumber g) {
     BigNumber y = boost::integer::mod_inverse(b, p);
-    if(y == 0 ) std::cerr<<"y is 0"<<std::endl;
-    std::cout<<"y: "<<y<<std::endl;
     BigNumber p_minus_one = p-1;
-    std::string H = sha256( boost::multiprecision::to_string(m));
+    std::string H = sha256( m);
     BigNumber inverted_s = boost::integer::mod_inverse(s, p_minus_one);
-    std::cout<<"s^-1: "<<inverted_s<<std::endl;
-    if(inverted_s == 0 ) std::cerr<<"inverted s is 0"<<std::endl;
     BigNumber numbered_hash = hex_to_bn(H);
     BigNumber u_one = (numbered_hash * inverted_s)%p_minus_one;
-    std::cout<<"u1: "<<u_one<<std::endl;
     BigNumber u_two = (r * inverted_s)%p_minus_one;
-    std::cout<<"u2: "<<u_two<<std::endl;
     BigNumber v = (pow(g, u_one) * pow(y, u_two))%p;
-    std::cout<<"v: "<<v<<std::endl;
     return v;
 }
 Encrypted encrypt_message(BigNumber g, BigNumber b, BigNumber m, BigNumber p) {
@@ -190,24 +166,19 @@ BigNumber decrypt_message(Encrypted encrypted_info, BigNumber a, BigNumber p) {
     return m;
 }
 int main() {
-    BigNumber p = 1000;
-    std::cout<<"p: "<<p<<std::endl;
-    BigNumber m = 2311;
-    std::cout<<"m: "<<m<<std::endl;
-    std::vector<BigNumber> roots = find_primitive_roots(p);
-    BigNumber g = roots[5];
-    std::cout<<"g: "<<g<<std::endl;
+    BigNumber p = 17;
+    std::string m = "231132323";
+    std::cout<<std::endl;
+    BigNumber g = find_primitive_roots(p)[0];
 
     BigNumber a = generate_private_key(p);
-    std::cout<<"a: "<<a<<std::endl;
     BigNumber b = generate_public_key(g,a,p);
-    std::cout<<"b: "<<b<<std::endl;
 
     Signing signing = sign_message(p, g, m, a);
     BigNumber v = verificate_message(b,p,m,signing.s,signing.r,g);
 
-    Encrypted encrypted_info = encrypt_message(g,b,m,p);
-    BigNumber decrypted = decrypt_message(encrypted_info, a, p);
+//    Encrypted encrypted_info = encrypt_message(g,b,m,p);
+//    BigNumber decrypted = decrypt_message(encrypted_info, a, p);
     bool is_verified = signing.r == v;
     std::cout<<"Verification: "<<is_verified<<std::endl;
     return 0;
